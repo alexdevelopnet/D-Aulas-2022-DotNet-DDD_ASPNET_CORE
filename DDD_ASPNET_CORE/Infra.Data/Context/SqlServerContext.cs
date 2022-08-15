dff3d93306ff241204c4e44cd7fa8c20614e194a
FamiliaFeitoza <FamiliaFeitoza@FEITOZA>
2022-08-14 21:55:50 -0300

    Base Construida

---
+++ DDD_ASPNET_CORE/Infra.Data/Context/SqlServerContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Infra.Data.Mapping;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infra.Data.Context
+{
+    public class SqlServerContext : DbContext
+    {
+        public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(new UserMap().Configure);
+        }
+    }
+}
